245f7280cc74c904577ec578ca7c2907aaa0f455
bezChalla <bchalla@gmu.edu>
2020-11-29 20:03:34 -0500

    initial styling
    meets all the requiermnets but can use more styling

--- EFSecurityShell/Controllers/HomeController.cs
+++ EFSecurityShell/Controllers/HomeController.cs
@@ -20,12 +20,5 @@ namespace Team2_AdmissionManagement.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
